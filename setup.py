6811745a9d6cb362cb3bd9c9ab37dc882545e161
William Ivanski <william.ivanski@gmail.com>
2017-11-03 16:20:39 -0200

    2.0: Support to special commands

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ rootdir = os.path.abspath(os.path.dirname(__file__))
 long_description = open(os.path.join(rootdir, 'README')).read()
 
 setup(name='Spartacus',
-      version='1.19',
+      version='2.0',
       description='Generic database wrapper',
       long_description=long_description,
       url='http://github.com/wind39/spartacus',
@@ -40,13 +40,13 @@ setup(name='Spartacus',
       packages=['Spartacus'],
       install_requires=['pyscrypt', 'pyaes', 'PrettyTable'],
       extras_require={
-        'postgresql': ['psycopg2'],
+        'postgresql': ['psycopg2', 'pgspecial'],
         'mysql':      ['PyMySQL'],
         'mariadb':    ['PyMySQL'],
         'firebird':   ['fdb'],
         'oracle':     ['cx_Oracle'],
         'mssql':      ['pymssql'],
         'ibmdb2':     ['ibm_db'],
-        'complete':   ['psycopg2', 'PyMySQL', 'fdb', 'cx_Oracle', 'pymssql', 'ibm_db']
+        'complete':   ['psycopg2', 'pgspecial', 'PyMySQL', 'fdb', 'cx_Oracle', 'pymssql', 'ibm_db']
       },
       zip_safe=False)

a3e81c4ff6b9a19e78d923b1a29f65e874adea03
William Ivanski <william.ivanski@gmail.com>
2018-04-04 14:48:49 -0300

    2.25: Fixed bug in pgspecial

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ rootdir = os.path.abspath(os.path.dirname(__file__))
 long_description = open(os.path.join(rootdir, 'README')).read()
 
 setup(name='Spartacus',
-      version='2.24',
+      version='2.25',
       description='Generic database wrapper',
       long_description=long_description,
       url='http://github.com/wind39/spartacus',

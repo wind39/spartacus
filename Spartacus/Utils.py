ca39ce8a295442d23a1b8f9d172efbef885ca67b
William Ivanski <william.ivanski@gmail.com>
2018-11-15 16:02:36 -0200

    2.74: Implemented Cryptor.Hash

--- Spartacus/Utils.py
+++ Spartacus/Utils.py
@@ -32,6 +32,7 @@ import openpyxl
 from collections import OrderedDict
 import tempfile
 import formulas
+import hashlib
 
 import Spartacus
 import Spartacus.Database
@@ -63,6 +64,11 @@ class Cryptor(object):
         try:
             v_aes = pyaes.AESModeOfOperationCTR(self.v_hash)
             return v_aes.decrypt(base64.b64decode(p_cyphertext)).decode(self.v_encoding)
+        except Exception as exc:
+            raise Spartacus.Utils.Exception(str(exc))
+    def Hash(self, p_text):
+        try:
+            return hashlib.md5(p_text.encode(self.v_encoding)).hexdigest()
         except Exception as exc:
             raise Spartacus.Utils.Exception(str(exc))
 
